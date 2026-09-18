98b0e3fea35459bd21a1304739c57510ab38e22c
James Molina Chirinos <jamesmch08@gmail.com>
2026-09-17 19:06:13 -0500

    chore(lint): ignorar dist en eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2021: true },
+  ignorePatterns: ['dist', 'node_modules'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
